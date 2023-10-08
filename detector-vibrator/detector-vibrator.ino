850f845ba78770838a410438bc1e2b29a659ce57
YourPalParanoid <98663544+YourPalParanoid@users.noreply.github.com>
2023-10-07 20:37:03 -0400

    All fingers

--- detector-vibrator/detector-vibrator.ino
+++ detector-vibrator/detector-vibrator.ino
@@ -6,12 +6,20 @@ const int vibration = 21;
 // sound speed in cm/ micro second
 #define SOUND_SPEED 0.034
 
-
 long duration;
 float distanceCm;
 
+int motorState = LOW;
+
 // function definition (prototype)
 boolean check_distance(float distance);
+
+// time (milliseconds) since last update
+unsigned long previousMillis = 0;
+
+const long scanInterval = 500; // (delay in milliseconds)
+long vibrateInterval = 1000;
+long vibrateMult = 1;
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,30 +31,36 @@ void setup() {
 }
 
 void loop() {
+  // unsigned long currentMillis = millis();
+  
   digitalWrite(trigger, LOW);
   delayMicroseconds(2);
 
   digitalWrite(trigger, HIGH);
-  delayMicroseconds(10);
   digitalWrite(trigger, LOW);
 
   duration = pulseIn(echo, HIGH);
 
   distanceCm = duration * SOUND_SPEED/2;
+  float currentDistance = distanceCm;
 
   if(check_distance(distanceCm)){
+    // motorState = HIGH;
+    delay(distanceCm *5);
     digitalWrite(vibration, HIGH);
+    Serial.println(distanceCm);
   }
   else{
+    // motorState = LOW;
     digitalWrite(vibration, LOW);
   }
-
+  
   Serial.print("distance cm: ");
   Serial.println(distanceCm);
-
-  delay(1000);
+  
+  delay(scanInterval);
 }
 
 boolean check_distance(float distance){
-  return distance>3 && distance<200;
+  return distance>3 && distance<20;
 }
